460d6ed09213bd89c4580c2ee72f904398b75c0a
Joan Barros <barros.joan@gmail.com>
2016-03-17 23:32:07 -0400

    Added custom label to the dropdowns in the NCF Number object

--- ncf_do/models/business_division.py
+++ ncf_do/models/business_division.py
@@ -7,3 +7,11 @@ class BusinessDivision(models.Model):
     code = fields.Char()
     name = fields.Char(string = "Name")
     description = fields.Char()
+
+    # Identifiers for Drop Downs
+    _rec_name='identifier'
+    identifier = fields.Char(compute='_gen_rec', hidden=True)
+    def _gen_rec(self):
+        name = self.name if self.name != False else ''
+        code = self.code if self.code != False else ''
+        self.identifier = name + ' [' + code + ']'
--- ncf_do/models/series.py
+++ ncf_do/models/series.py
@@ -4,6 +4,15 @@ from openerp import models, fields, api
 
 class Series(models.Model):
     _name = 'ncf_do.series'
+    _rec_name='code'
     code = fields.Char(string = "Name")
     name = fields.Char(string = "Code")
     description = fields.Char()
+
+    # Identifiers for Drop Downs
+    _rec_name='identifier'
+    identifier = fields.Char(compute='_gen_rec', hidden=True)
+    def _gen_rec(self):
+        name = self.name if self.name != False else ''
+        code = self.code if self.code != False else ''
+        self.identifier = name + ' [' + code + ']'
