9aa9ad1fc59012dd7f3e9395e13816f74396f285
Joan Barros <barros.joan@gmail.com>
2016-03-17 20:21:17 -0400

    Fixed prefix field generation

--- ncf_do/models/ncf.py
+++ ncf_do/models/ncf.py
@@ -8,13 +8,16 @@ class NCF(models.Model):
     description = fields.Char()
     prefix = fields.Char(compute='compute_prefix')
 
-    series = fields.Many2one('ncf_do.series', ondelete='set null', delegate=True)
+    series = fields.Many2one('ncf_do.series', ondelete='set null')
     business_division = fields.Many2one('ncf_do.business_division', ondelete='set null')
     emission_point = fields.Many2one('ncf_do.emission_point', ondelete='set null')
     impression_area = fields.Many2one('ncf_do.impression_area', ondelete='set null')
 
     def compute_prefix(self):
-        self.prefix = self.series \
-                    + self.business_division \
-                    + self.emmision_point \
-                    + self.impression_area
+        if (self.series.code == False or self.business_division.code == False or self.emission_point.code == False or self.impression_area.code == False):
+            self.prefix = 'Not Available'
+        else:
+            self.prefix = str(self.series.code) \
+                       + str(self.business_division.code) \
+                        + str(self.emission_point.code) \
+                        + str(self.impression_area.code)
