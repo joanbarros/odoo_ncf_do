17275c5e21fb53631aeddb2ca781251063e56186
Joan Barros <barros.joan@gmail.com>
2016-11-07 23:48:26 -0400

    Adding a bunch of configuration changes and features. Docker Composer support and a work in progres Vagrant File.

---
+++ Dockerfile
@@ -0,0 +1,1 @@
+RUN echo 'this is a test docker file.'
--- addons/ncf_do_pos/__openerp__.py
+++ addons/ncf_do_pos/__openerp__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'ncf_do'],
+    'depends': ['base', 'ncf_do', 'point_of_sale'],
 
     # always loaded
     'data': [
