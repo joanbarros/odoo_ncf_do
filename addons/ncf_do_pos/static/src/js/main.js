37809293d5283a861620e8e559a012ca24042bb4
Joan Barros <barros.joan@gmail.com>
2017-02-03 00:24:24 -0400

    Added the RCN field to the client selection screen

--- addons/ncf_do_pos/static/src/js/main.js
+++ addons/ncf_do_pos/static/src/js/main.js
@@ -10,6 +10,19 @@ odoo.define('ncf_do_pos.main', function (require) {
     }
   });
 
+  // Add the field to the model so it's pulled form the db
+  var pos_models = require('point_of_sale.models');
+  console.log('pos_models', pos_models);
+  var models = pos_models.PosModel.prototype.models;
+  for (var i = 0; i < models.length; i++) {
+    if (models[i].model == 'res.partner') {
+      console.log('fields', models[i].fields);
+      models[i].fields.push('rnc');
+      console.log('Added rnc field to the partner field list');
+    }
+  }
+  console.log('newPosModel', pos_models.PosModel);
+
   return {
     Mine: mine
   };
